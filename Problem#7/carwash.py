94931e425dda94c4545b86528653b011e792cc18
rileyspollard <rileyspollard@outlook.com>
2024-02-28 16:34:10 -0600

    problem 7

--- Problem#7/carwash.py
+++ Problem#7/carwash.py
@@ -4,11 +4,26 @@
 def calculate_car_wash_price(service_choice1, service_choice2):
     services = {'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5}
     base_wash = 10
-    total = 0
+    total = base_wash
+    print('ZyCar Wash\nBase car wash - $10')
+    
    
    #type your code here 
-
+    if service_choice1 != '-':
+        if service_choice1 in services:
+            total += services[service_choice1]
+            print(service_choice1 + ' - $' + str(services[service_choice1]))
+        else:
+            print('Invalid service choice: ' + service_choice1)
     
+    if service_choice2 != '-':
+        if service_choice2 in services:
+            total += services[service_choice2]
+            print(service_choice2 + ' - $' + str(services[service_choice2]))
+        else:
+            print('Invalid service choice: ' + service_choice2)
+    
+    print('-----\nTotal price: $' + str(total))
 if __name__ == '__main__':
     # Get user input for service choices
     service_choice1 = input("Enter first service choice: ")
